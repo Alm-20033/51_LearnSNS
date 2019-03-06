d29cdece9ec3469248fefab4ffb227019f6b23d5
alm120033 <kinjoaimi1@gmail.com>
2019-03-06 16:04:09 +0900

    変更

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,8 @@
 <?php
-$dsn = 'mysql:dbname=;host=';
-$user = '';
+$dsn = 'mysql:dbname=learnsns;host=localhost';
+$user = 'root';
 $password='';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->query('SET NAMES utf8');
+
